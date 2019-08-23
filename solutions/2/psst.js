1ce55a90cca6a7ac68fb8bc10c7b5fde37d2e251
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-08-24 02:30:32 +0300

    initial commit

---
+++ solutions/2/psst.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const crypto = require('crypto');
+
+module.exports = (secret) => {
+  if (!secret || typeof secret !== 'string') {
+    throw Error('invalid secret!');
+  }
+
+  const functions = {
+    sign: (value) => {
+      return crypto.createHmac('sha256', secret).update(value).digest('hex');
+    },
+    validate: (value, hash) => {
+      const correctHash = functions.sign(value);
+      return correctHash === hash;
+    }
+  };
+
+  return functions;
+}
